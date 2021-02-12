9ad798322c949742fe333f99f22892e646c1da77
Leonidas512 <74636943+Leonidas512@users.noreply.github.com>
2021-02-11 21:04:08 -0600

    SECURITY UPGRADE
    To secure page add this to html
    
    <script src="js/security.js"></script>

---
+++ js/security.js
@@ -0,0 +1,17 @@
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+today = mm + '/' + dd + '/' + yyyy;
+var todaykey = CryptoJS.AES.encrypt(today, "gofalcons123");
+
+if (localStorage.getItem("var") === null) {
+  window.location.replace("/");
+}
+
+if (newkey != key) {
+  alert("please log in")
+  window.location.replace("/");
+}
+
+
